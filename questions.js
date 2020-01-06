6831b0b730d57aa321f8b4a8f1ee39ab5d596962
iskona <isony.d@gmail.com>
2020-01-05 23:38:24 -0800

    added a couple of questions to questions array

--- questions.js
+++ questions.js
@@ -1,8 +1,8 @@
 let questions = [
   {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    title: "What is the name of a JavaScript library designed to simplify HTML DOM tree traversal and manipulation, as well as event handling, CSS animation, and Ajax?",
+    choices: ["Bootstrap", "Angular", "Node.js", "jQuery"],
+    answer: "jQuery"
   },
   {
     title: "Variables wich are declared within a function, as well as the function parameters have _____ scope.",
@@ -11,12 +11,17 @@ let questions = [
   },
   {
     title: "Javascript written in the HTML page needs to be contained in what tag?",
-    choices: ["<java>", "<script>", "<p>", "<javascript>"],
+    choices: ["<java>", "<p>", "<script>", "<javascript>"],
     answer: "<script>"
   },
   {
     title: "What statement you can use to send a value back out of a function?",
     choices: ["return", "class", "functionClass", "for"],
     answer: "return"
+  },
+  {
+    title: "The keyword ____ tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses.",
+    choices: ["null", "none", "bool", "if"],
+    answer: "if"
   }
 ];
